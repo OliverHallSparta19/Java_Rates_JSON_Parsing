739591815be75a370329b9c14b210bf90b5db5b8
Oliver <OHall@spartaglobal.com>
2018-11-05 16:00:42 +0000

    Adding missing files

---
+++ src/main/java/RatesMain.java
@@ -0,0 +1,9 @@
+public class RatesMain {
+    public static void main(String args[]){
+
+        RatesFileReader ratesFileReader = new RatesFileReader("Resources/rates.json");
+        System.out.println(ratesFileReader);
+
+    }
+}
+
